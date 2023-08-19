a7401be9df4a89e9dd3a909932a5517a322ff75a
Velocity07 <87845181+Velocity07@users.noreply.github.com>
2023-08-19 22:02:01 +0530

    Create basic_slash_command.py

---
+++ commands/basic_slash_command.py
@@ -0,0 +1,18 @@
+import lightbulb, hikari
+
+plugin = lightbulb.Plugin('basic_slash_command')
+
+def load(bot):
+  bot.add_plugin(plugin)
+
+@plugin.listener(hikari.GuildMessageCreateEvent)
+async def message(event):
+  print(event.content)
+
+@plugin.command
+@lightbulb.command('your_command_name', 'your_command_description')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Your_Command_Response')
+
+#Coded by Velocity7
