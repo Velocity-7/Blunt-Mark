568cf83619483080972715c38c85e0aa8cc89c63
Velocity07 <87845181+Velocity07@users.noreply.github.com>
2023-08-19 21:55:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import lightbulb
 bot = lightbulb.BotApp(token='YOUR_BOT_TOKEN',
 #Required ^                    
     default_enabled_guilds=(YOUR_GUILD_ID))
-#Too add more than one guild use: (YOUR_GUILD_ID, ANOTHER_GUILD_ID) (Used for loading commands faster in guilds) 
+#Too add more than one guild use: (YOUR_GUILD_ID, ANOTHER_GUILD_ID) (Used for loading commands faster in guilds)
+
+#Load Commands
+bot.load_extensions_from('./commands')
     
 @bot.command
 @lightbulb.command('your_command_name', 'your_command_description')
