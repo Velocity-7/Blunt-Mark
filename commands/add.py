aeb90919a36d59fb8f11dcb618614bffd5e83cbb
Velocity07 <vihaanbhargav@icloud.com>
2023-09-25 22:10:11 +0530

    update

---
+++ commands/add.py
@@ -0,0 +1,21 @@
+import lightbulb, hikari
+
+plugin = lightbulb.Plugin('add')
+
+def load(bot):
+  bot.add_plugin(plugin)
+
+@plugin.listener(hikari.GuildMessageCreateEvent)
+async def message(event):
+  print(event.content)
+
+@plugin.command
+@lightbulb.option('num_1', 'Number_1',int)
+@lightbulb.option('num_2', 'Number_2',int)
+@lightbulb.command('add', 'Add_2_numbers_ together')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx):
+  sum = ctx.options.num1 + ctx.options.num2
+  await ctx.respond(str(sum))
+
+#Coded by Velocity7
