dce0200dc1eb68837dd04e027db9098508ee4616
Velocity07 <vihaanbhargav@icloud.com>
2023-09-27 20:00:19 +0530

    Update master.py

--- master.py
+++ master.py
@@ -1,12 +1,17 @@
 #Run 'python -m pip install -U hikari' before you start
 #Run 'pip install hikari-lightbulb' before you start
 
-import lightbulb
+import lightbulb, hikari
 
 bot = lightbulb.BotApp(token='YOUR_BOT_TOKEN',
 #Required ^                    
     default_enabled_guilds=(YOUR_GUILD_ID))
 #Too add more than one guild use: (YOUR_GUILD_ID, ANOTHER_GUILD_ID) | (Used for loading commands faster in guilds)
+
+@bot.listen(hikari.StartedEvent)
+async def start(event):
+ await bot.update_presence(status=hikari.Status.ONLINE)
+#Changes the bot status ||DND, IDLE, ONLINE|| ^
 
 bot.load_extensions_from('./commands')
 #Loads Commands ^
