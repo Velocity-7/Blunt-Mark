df6095206a505ae5f9ad27ae7ee987092de3bc61
Velocity07 <87845181+Velocity07@users.noreply.github.com>
2023-11-14 15:25:50 +0530

    Update embed_command.py

--- commands/embed_command.py
+++ commands/embed_command.py
@@ -10,12 +10,12 @@ async def message(event):
   print(event.content) 
 
 @plugin.command
-@lightbulb.command('your_command_field', 'Your_Context_Field')
+@lightbulb.command('your_embed_command_name_field', 'Your_Embed_Command_Context_Field')
 @lightbulb.implements(lightbulb.SlashCommand)
-async def embed_command(ctx: lightbulb.Context) -> None:
+async def embed_command_name(ctx: lightbulb.Context) -> None:
     embed = hikari.Embed(title='Your_Title', description='Your_Description')
     embed.add_field('Your_Field_Name', 'Your_Field_Context')
-    embed.set_thumbnail("https:https://example.com/")
+    embed.set_thumbnail("https://example.com/")
     embed.set_footer('Your_Footer')
     await ctx.respond(embed)
 #Creates the embed ^
