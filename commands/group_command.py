7326e4df2fd533cd45a380762aed135b1242e368
Velocity07 <87845181+Velocity07@users.noreply.github.com>
2023-11-14 15:20:28 +0530

    Update group_command.py

--- commands/group_command.py
+++ commands/group_command.py
@@ -10,16 +10,16 @@ async def message(event):
   print(event.content) 
 
 @plugin.command
-@lightbulb.command('your_group_command_field', 'Your_Group_Command_Context_Field')
+@lightbulb.command('your_group_name_field', 'Your_Group_Context_Field')
 @lightbulb.implements(lightbulb.SlashCommandGroup)
 async def your_group_name(ctx):
   pass
 #Creates the parent group ^
 
 @your_group_name.child
-@lightbulb.command('your_sub_command_field', 'Your_Sub_Command_Context_Field')
+@lightbulb.command('your_sub_command_name_field', 'Your_Sub_Command_Context_Field')
 @lightbulb.implements(lightbulb.SlashSubCommand)
-async def embed_command(ctx: lightbulb.Context) -> None:
+async def your_sub_command_name(ctx: lightbulb.Context) -> None:
     embed = hikari.Embed(title='Your_Title', description='Your_Description')
     embed.add_field('Your_Field_Name', 'Your_Field_Context')
     embed.set_thumbnail("https:https://example.com/")
@@ -28,9 +28,9 @@ async def embed_command(ctx: lightbulb.Context) -> None:
 #Creates the child sub-command ^
 
 @your_group_name.child
-@lightbulb.command('your_sub_command_field', 'Your_Sub_Command_Context_Field')
+@lightbulb.command('your_sub_command_name_field_2', 'Your_Sub_Command_Context_Field')
 @lightbulb.implements(lightbulb.SlashSubCommand)
-async def ping(ctx):
+async def your_sub_command_name_2(ctx):
     await ctx.respond('Your_Response')
 #Creates the child sub-command ^
 
